171d74ebc7099f0875228246e587cce9b126ec82
Ahmad Darmawan <adarwawan@gmail.com>
2015-09-23 23:10:45 +0700

    Commit Transmitter
    Masih salah threadnya

--- transmitterwew.cpp
+++ transmitterwew.cpp
@@ -11,14 +11,62 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <fstream>
+#include <pthread.h>
+
+#define NUM_THREADS 2;
+
+/* XON/XOFF protocol */
+#define XON (0x11)
+#define XOFF (0x13)
+
+void *signalXonXoff(int fd, char buf, (struct sockaddr *)&remaddr, int &slen)
+{
+	do {
+		recvlen = recvfrom(fd, buf, 1, 0, (struct sockaddr *)&remaddr, &slen);
+	} while (!done);
+	pthread_exit(NULL);
+}
+
+void *sendData(string file_teks, int fd, char buf, char bufData, (struct sockaddr *)&remaddr, int &slen)
+{
+	ifstream fin;
+   	char current;
+	fin.open(file_teks, ios::in);
+	while (!fin.eof()) {
+		if (buf != XOFF)
+		{
+			fin.get(current);
+			bufData = current;
+			if (sendto(fd, bufData, strlen(bufData), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+				perror("sendto");
+			}
+			{
+			else
+				//print data ini kekirim
+			}
+		}
+		else {
+			// print kena XOFF
+			while (buf != XON) do {
+				// print menunggu XON
+			}
+			// sudah XON
+		}
+	}
+	done = true;
+	pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[])
 {
-	struct sockaddr_in serv_addr;
+	struct sockaddr_in serv_addr, remaddr;
     struct hostent *server;
+    int slen = sizeof(remaddr);
+    char buf[1], bufData[1];
+    bool done = false;
 
 	if (argc < 4) {
-		cerr << "Masukan Salah" << endl;
+		perror("Masukan salah\n");
 	}
 	else { //argc == 3
 		int portno = atoi(argv[2]);
@@ -27,43 +75,46 @@ int main(int argc, char *argv[])
 		// Membuat socket
 		int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     	if (sockfd < 0) 
-        	cout << "ERROR opening socket" << endl;
+        	fprintf(stderr,"ERROR, opening socket\n");
     		server = gethostbyname(argv[1]);
     	if (server == NULL) {
         	fprintf(stderr,"ERROR, no such host\n");
         	exit(0);
     	}
 
-		// Membuat fork
-    	int counter = 0;
-    	pid_t pid = fork();
+    	// Membuat bind
+    	memset((void *)&serv_addr, 0, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		serv_addr.sin_port = htons(0);
 
-    	if (pid == 0)
-    	{
-        	/* repeat  
-        		ch <- read a character from socket  
-        		last received char <- ch
-        	until connection is terminated */
-    	}
-    	else if (pid > 0)
-    	{
-    		ifstream fin;
-    		char current;
-    		fin.open(argv[3], ios::in);
-    		while (!fin.eof()) {
-    			if (/*ini gimana*/) {
-    				fin.get(current);
-    				//send current to socket
-    			}
-    		}
-    	}
-    	else
-    	{
-    	    // fork failed
-    	    printf("fork() failed!\n");
-    	    return 1;
-    	}
+		if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+			perror("bind failed");
+			return 1;
+		}
+
+		alen = sizeof(serv_addr);
+		if (getsockname(sockfd, (struct sockaddr *)&serv_addr, &alen) < 0) {
+			perror("getsockname failed");
+			return 1;
+		}
+
+		printf("bind complete. Port number = %d\n", ntohs(serv_addr.sin_port));
+
+    	memset((char *) &remaddr, 0, sizeof(remaddr));
+		remaddr.sin_family = AF_INET;
+		remaddr.sin_port = htons(portno);
+		if (inet_aton(server, &remaddr.sin_addr)==0) {
+			fprintf(stderr, "inet_aton() failed\n");
+			exit(1);
+		}
+
+
+    	// Membuat thread
+    	pthread_t threads[NUM_THREADS];
 
 	}
+
+	close(sockfd);
 	return 0;
 }
